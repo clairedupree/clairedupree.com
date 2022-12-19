47cebd25d526f1449a3d89ce117f3f457e4e7c7b
Claire Dupree <claire.e.dupree@gmail.com>
2022-12-18 20:25:55 -0500

    Clean up comments, ready for submit;

--- clairedupree-script.js
+++ clairedupree-script.js
@@ -1,9 +1,9 @@
+// Claire Dupree
+// 12/18/22
+
 "use strict";
-// const homeHeaders = document.querySelector(".home-main");
-// homeHeaders.style.color = "red";
 const initialOpacity = 0.8;
 const galleryImages = $("#gallery-images");
-// const homeSection = $("#home");
 const homeLink = $("#home-link");
 
 // select nav jump links, add scroll eventlistener
@@ -17,15 +17,14 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-// $(".home-main").style.color = "red";
-// homeHeaders.firstChild.style.color = "blue";
 // add a hover effect for each letter
 $(document).ready(() => {
+  // hide home link in nav on load
   homeLink.hide();
 
+  // fade home link in/out if homepage is visible
   document.addEventListener("scroll", (e) => {
     console.log("scroll");
-    // console.log($("#tech").scrollTop());
 
     if ($(window).scrollTop() > 800) {
       homeLink.fadeIn();
@@ -34,21 +33,7 @@ $(document).ready(() => {
     }
   });
 
-  // $(document).ready(() => {
-  //   document.addEventListener("scroll", (e) => {
-  //     console.log("scroll");
-  //     if ($("#home").is(":visible")) {
-  //       homeLink.hide();
-  //     } else {
-  //       homeLink.show();
-  //     }
-  //   });
-
-  // $(".home-header span").css("opacity", initialOpacity);
-  // $(".home-header span").hover(
-  //   () => console.log("in"),
-  //   () => console.log("out")
-  // );
+  // animate text on hover
   $(".hover-animate-sect span").hover(
     function () {
       $(this).animate({ fontSize: "7rem" }, 150);
@@ -58,20 +43,14 @@ $(document).ready(() => {
     }
   );
   $(".hover-animate-h1 span").hover(
-    // $(".home-header span").hover(
     function () {
-      // $(this).animate({ opacity: 0.8 }, 150);
       $(this).animate({ fontSize: "9rem" }, 150);
-      // $(this).css("color", "brown");
     },
     function () {
-      // $(this).animate({ opacity: 1 }, 150);
       $(this).animate({ fontSize: "10rem" }, 150);
-      // $(this).css("color", "red");
     }
   );
   $(".hover-animate-h2 span").hover(
-    // $(".home-subheader span").hover(
     function () {
       $(this).animate({ fontSize: "2.8rem" }, 150);
     },
@@ -79,21 +58,4 @@ $(document).ready(() => {
       $(this).animate({ fontSize: "3rem" }, 150);
     }
   );
-  // $(".socials span").hover(
-  //   function () {
-  //     $(this).animate({ fontSize: "1.45rem" }, 150);
-  //   },
-  //   function () {
-  //     $(this).animate({ fontSize: "1.5rem" }, 150);
-  //   }
-  // );
-
-  // $("#test").hover(
-  //   function () {
-  //     $("#test").animate({ fontsize: "100px" }, 300);
-  //   },
-  //   function () {
-  //     $("#test").animate({ fontsize: "10px" }, 300);
-  //   }
-  // );
 });
